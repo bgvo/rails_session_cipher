6426da3a2fd007b551c67c3cd39856bc7b66b6a4
Borja GVO <borjavinuessa@gmail.com>
2023-12-21 19:26:43 +0100

    Refactor

--- lib/rails_session_cipher.rb
+++ lib/rails_session_cipher.rb
@@ -12,28 +12,18 @@ module RailsSessionCipher
 
   class << self
     def encrypt(data, key, iv, auth_tag, options = {})
-      # Convert data to JSON
       json_data = data.to_json
+      cipher, secret = initialize_cipher(key, options, "encrypt")
 
-      # Key generation with PBKDF2
-      cipher = OpenSSL::Cipher.new("aes-256-gcm")
-      iteration_count = configuration.iteration_count
-      salt = options[:salt] || configuration.salt
-      hash_digest_class = options[:hash_digest_class] || configuration.hash_digest_class
-      secret = OpenSSL::PKCS5.pbkdf2_hmac(key, salt, iteration_count, cipher.key_len, hash_digest_class.new)
-
-      # Initialize cipher for encryption
       cipher.encrypt
       cipher.key = secret
       iv = cipher.random_iv
       cipher.iv = iv
       cipher.auth_data = ""
 
-      # Encrypt the data
       encrypted_data = cipher.update(json_data) + cipher.final
       auth_tag = cipher.auth_tag
 
-      # Encode and concatenate components
       [Base64.strict_encode64(encrypted_data), Base64.strict_encode64(iv), Base64.strict_encode64(auth_tag)].join("--")
     end
 
@@ -41,26 +31,30 @@ module RailsSessionCipher
       data, iv, auth_tag = session_cookie&.split("--")&.map { |v| Base64.strict_decode64(v) }
       raise InvalidMessage if (auth_tag.nil? || auth_tag.bytes.length != 16)
 
-      salt = options[:salt] || configuration.salt
-      hash_digest_class = options[:hash_digest_class] || configuration.hash_digest_class
+      cipher, secret = initialize_cipher(key, options, "decrypt")
 
-      cipher = OpenSSL::Cipher.new("aes-256-gcm")
-      iteration_count = configuration.iteration_count
-      secret = OpenSSL::PKCS5.pbkdf2_hmac(key, salt, iteration_count, cipher.key_len, hash_digest_class.new)
-
-      # Setup cipher for decryption and add inputs
       cipher.decrypt
       cipher.key = secret
       cipher.iv = iv
       cipher.auth_tag = auth_tag
       cipher.auth_data = ""
 
-      # Perform decryption
       cookie_payload = cipher.update(data)
       cookie_payload << cipher.final
       JSON.parse(cookie_payload)
     rescue InvalidMessage
       {}
+    end
+
+    private
+
+    def initialize_cipher(key, options, mode)
+      cipher = OpenSSL::Cipher.new("aes-256-gcm")
+      iteration_count = configuration.iteration_count
+      salt = options[:salt] || configuration.salt
+      hash_digest_class = options[:hash_digest_class] || configuration.hash_digest_class
+      secret = OpenSSL::PKCS5.pbkdf2_hmac(key, salt, iteration_count, cipher.key_len, hash_digest_class.new)
+      [cipher, secret]
     end
   end
 end
